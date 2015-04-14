f115f648b4d254053cb76afccb79d7b59c001522
bengal2009 <bennylin@outlook.com>
2015-04-14 17:13:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/blin/updatesample/NetworkTool.java
@@ -0,0 +1,46 @@
+package com.example.blin.updatesample;
+
+/**
+ * Created by blin on 2015/4/14.
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+public class NetworkTool {
+
+    /**
+     * Æq®p???â­îØ?ü¸
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public static String getContent(String url) throws Exception{
+        StringBuilder sb = new StringBuilder();
+
+        HttpClient client = new DefaultHttpClient();
+        HttpParams httpParams = client.getParams();
+        //æ¦?Ã¦?Ýr????îíØQáø
+        HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+        HttpConnectionParams.setSoTimeout(httpParams, 5000);
+        HttpResponse response = client.execute(new HttpGet(url));
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent(), "UTF-8"), 8192);
+
+            String line = null;
+            while ((line = reader.readLine())!= null){
+                sb.append(line + "\n");
+            }
+            reader.close();
+        }
+        return sb.toString();
+    }
+}
